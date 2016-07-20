620ccc9e6c66ece94111e006a65b8c3b42e95377
Ted Yapo <ted.yapo@zednaughtlabs.com>
2016-07-20 10:19:07 -0400

    updated version number to sync with pipy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'loopfield',
   packages = ['loopfield'],
-  version = '1.0.2',
+  version = '1.0.4',
   description = 'Current loop magnetic field calculator',
   author = 'Ted Yapo',
   author_email = 'ted.yapo@zednaughtlabs.com',
